2d0b3b38581e4a04809eaa7bdeef0e7d9762fceb
yogeshwilliam <yogeeshwaran06@gmail.com>
2023-01-08 02:57:33 +0530

    inital commit

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended:false}));
+
+// set up multer
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null,'uploadedFiles')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+//create multer instance
+var upload = multer({ storage: storage })
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {   
+  res.status(200).json({"name":req.file.originalname,"type":req.file.mimetype,"size":req.file.size});
+})
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port- localhost:' + port)
 });
